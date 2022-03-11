070db67b39ca1ce79a3099911faab9f63bde558a
Artem Kozlov <satisataka@gmail.com>
2022-03-11 19:39:20 +0300

    OpenGraph

--- tafos/settings.py
+++ tafos/settings.py
@@ -42,6 +42,7 @@ MIDDLEWARE = [
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 	'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+	'django.contrib.sites.middleware.CurrentSiteMiddleware',
 ]
 
 CACHES = {

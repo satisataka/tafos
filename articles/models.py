07ca4d04ce45e161b220826f3602df128525bc0b
Artem Kozlov <satisataka@gmail.com>
2022-03-15 23:44:36 +0300

    articles model fix

--- articles/models.py
+++ articles/models.py
@@ -66,9 +66,8 @@ class Article(models.Model):
 	user = models.ForeignKey(
 		settings.AUTH_USER_MODEL,
 		on_delete=models.SET(get_sentinel_user),
-		default=get_user_model().objects.get_or_create(username='admin')[0],
-		null=False,
-		blank=False,
+		default=None,
+		null=True,
 		verbose_name='Пользователь',
 	)
 

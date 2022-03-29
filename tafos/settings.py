e7b6222f76a754fc9e384cd8cc448a8d1e8912f0
Artem Kozlov <satisataka@gmail.com>
2022-03-30 00:41:38 +0300

    :)

--- tafos/settings.py
+++ tafos/settings.py
@@ -100,11 +100,14 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = 'ru'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
+
 TIME_ZONE = 'Europe/Moscow'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 

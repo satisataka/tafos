b0c4a568a0c14d66f84ae8655994ea7271b380ce
Artem Kozlov <satisataka@gmail.com>
2022-03-30 00:43:10 +0300

    add status code for error pages

--- main/views.py
+++ main/views.py
@@ -49,16 +49,16 @@ class СarouselView(TemplateView):
 
 
 def custom_page_not_found_view(request, exception):
-	return render(request, "errors/404.html", {})
+	return render(request, "errors/404.html", status=404)
 
 
 def custom_error_view(request, exception=None):
-	return render(request, "errors/500.html", {})
+	return render(request, "errors/500.html", status=500)
 
 
 def custom_permission_denied_view(request, exception=None):
-	return render(request, "errors/403.html", {})
+	return render(request, "errors/403.html", status=403)
 
 
 def custom_bad_request_view(request, exception=None):
-	return render(request, "errors/400.html", {})
+	return render(request, "errors/400.html", status=400)

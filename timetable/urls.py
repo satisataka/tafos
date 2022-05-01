4729d8565286c59078d295a3388c47d3619f0d11
Artem Kozlov <satisataka@gmail.com>
2022-05-01 21:04:47 +0300

    ))

--- timetable/urls.py
+++ timetable/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
-from datetime import date
 
 from .views import TimetableWeekArchiveView
 
 app_name = "timetable"
 
+
 urlpatterns = [
-	path('', TimetableWeekArchiveView.as_view(
-		year=int(date.today().year),
-		week=int(date.today().isocalendar()[1])),
-		name='timetable_now'),
+	path('', TimetableWeekArchiveView.as_view(), name='timetable_now'),
 	path('<int:year>/week/<int:week>', TimetableWeekArchiveView.as_view(), name='timetable'),
 ]
--- timetable/views.py
+++ timetable/views.py
@@ -1,4 +1,6 @@
 from django.views.generic.dates import WeekArchiveView
+from django.http import Http404
+from django.utils.timezone import now
 from .models import TimeTable
 
 
@@ -8,6 +10,21 @@ class TimetableWeekArchiveView(WeekArchiveView):
 	week_format = "%W"
 	date_field = 'day'
 	allow_empty = True
+
+	def get_week(self):
+		try:
+			month = super().get_week()
+		except Http404:
+			month = now().strftime(self.get_week_format())
+		return month
+
+	def get_year(self):
+		try:
+			year = super().get_year()
+		except Http404:
+			year = now().strftime(self.get_year_format())
+
+		return year
 
 	def get_ordering(self):
 		ordering = self.request.GET.get('-day')
